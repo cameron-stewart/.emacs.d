ece4196a0fe3fe8313d3f6e1db5ec6cc78d1dd16
cameron-stewart <cameron.n.stewart@gmail.com>
2017-10-22 12:35:39 +0200

    Added better-defaults

--- init.el
+++ init.el
@@ -11,13 +11,17 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal)))))
 
-
+;; Add and enable solarized color scheme
 (add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
 (require 'color-theme)
 (add-to-list 'load-path "~/.emacs.d/emacs-color-theme-solarized")
 (require 'color-theme-solarized)
-
 (color-theme-solarized)
 
+;; Enable ido
 (require 'ido)
 (ido-mode t)
+
+;; Add and enable better-defaults
+(add-to-list 'load-path "~/.emacs.d/better-defaults")
+(require 'better-defaults)
