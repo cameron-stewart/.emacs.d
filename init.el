eedb581d176c9dfb9dfa8d06523265afb5bfa37d
cameron-stewart <cameron.n.stewart@gmail.com>
2017-10-25 10:47:11 -0400

    Modified some settings to use LaTeX

--- init.el
+++ init.el
@@ -29,3 +29,32 @@
 ;; Add line numbers in the margin
 (global-linum-mode t)
 
+;; Enable AUCTeX plugin
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-save-query nil)
+(setq TeX-PDF-mode t)
+
+;; Enable flymake syntax check for latex
+(require 'flymake)
+
+(defun flymake-get-tex-args (file-name)
+(list "pdflatex"
+(list "-file-line-error" "-draftmode" "-interaction=nonstopmode" file-name)))
+
+(add-hook 'LaTeX-mode-hook 'flymake-mode)
+
+;; Enable spell checking in LaTeX mode
+(setq ispell-program-name "aspell") ; could be ispell as well, depending on your preferences
+(setq ispell-dictionary "english") ; this can obviously be set to any language your spell-checking program supports
+
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-buffer)
+
+;; Enable LaTeX outline minor mode
+(defun turn-on-outline-minor-mode ()
+(outline-minor-mode 1))
+
+(add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
+(add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
+(setq outline-minor-mode-prefix "\C-c \C-o")
